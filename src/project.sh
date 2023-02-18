7febdf3b2595b1db4a477131340e39d634d03558
Scott Leberknight <174812+sleberknight@users.noreply.github.com>
2023-02-17 22:05:05 -0500

    Avoid costly repeated setups
    * Add a server.bats test that starts and stops ncat
    * Use setup_file and teardown_file to do setup/teardown once

--- src/project.sh
+++ src/project.sh
@@ -7,5 +7,20 @@ if [[ ! -e "$FIRST_RUN_FILE" ]]; then
     touch "$FIRST_RUN_FILE"
 fi
 
-echo "NOT IMPLEMENTED!" >&2
-exit 1
+case $1 in
+    start-echo-server)
+        echo "Starting echo server"
+        PORT=2000
+        ncat -l $PORT -k -c 'xargs -n1 echo' 2>/dev/null & # don't keep open this script's stderr
+        echo $! > /tmp/project-echo-server.pid
+        echo "$PORT" >&2
+    ;;
+    stop-echo-server)
+        kill "$(< "/tmp/project-echo-server.pid")"
+        rm /tmp/project-echo-server.pid
+        ;;
+    *)
+        echo "NOT IMPLEMENTED!" >&2
+        exit 1
+    ;;
+esac
---
+++ test/server.bats
@@ -0,0 +1,14 @@
+setup_file() {
+    load 'test_helper/common-setup'
+    _common_setup
+    PORT=$(project.sh start-echo-server 2>&1 >/dev/null)
+    export PORT
+}
+
+teardown_file() {
+    project.sh stop-echo-server
+}
+
+@test "server is reachable" {
+    nc -z localhost "$PORT"
+}
